c5ee295d2bb6f2bc7413725bec38272375ef0a83
Connor Walker <connorjwalker32@hotmail.co.uk>
2023-02-27 23:44:32 +0000

    added form validation

--- StocksCompetition/Shared/LogInForm.cs
+++ StocksCompetition/Shared/LogInForm.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace StocksCompetition.Shared;
 
 public class LogInForm
 {
+    [Required(ErrorMessage = "The Discord Username Field is Required")]
     public string DiscordUsername { get; set; } = string.Empty;
 
+    [Required(ErrorMessage = "The Discord Username Field is Required")]
     public string Password { get; set; } = string.Empty;
 }
--- StocksCompetition/Shared/SignUpForm.cs
+++ StocksCompetition/Shared/SignUpForm.cs
@@ -1,17 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace StocksCompetition.Shared;
 
 public class SignUpForm
 {
     public string ProfilePicture { get; set; } = string.Empty;
 
+    [Required(ErrorMessage = "The Display Name Field is Required")]
     public string DisplayName { get; set; } = string.Empty;
 
+    [Required(ErrorMessage = "The Discord Username Field is Required")]
     public string DiscordUsername { get; set; } = string.Empty;
 
+    [Required(ErrorMessage = "The Display Colour is Required")]
     public string DisplayColour { get; set; }
-
+    
+    [Required(ErrorMessage = "The Password Field is Required")]
     public string Password { get; set; } = string.Empty;
 
+    [Required(ErrorMessage = "The Confirm Password Field is Required")]
+    [Compare("Password", ErrorMessage = "Passwords Must Match")]
     public string ConfirmPassword { get; set; } = string.Empty;
 
     public string FreetradeCookie { get; set; } = string.Empty;
