e4b3a38811139b10a676d38d05993aa724c68d45
Connor Walker <connorjwalker32@hotmail.co.uk>
2023-02-27 22:52:26 +0000

    added log in form

---
+++ StocksCompetition/Shared/LogInForm.cs
@@ -0,0 +1,8 @@
+namespace StocksCompetition.Shared;
+
+public class LogInForm
+{
+    public string DiscordUsername { get; set; } = string.Empty;
+
+    public string Password { get; set; } = string.Empty;
+}
--- StocksCompetition/Shared/SignUpForm.cs
+++ StocksCompetition/Shared/SignUpForm.cs
@@ -1,6 +1,6 @@
 namespace StocksCompetition.Shared;
 
-public class SignUp
+public class SignUpForm
 {
     public string ProfilePicture { get; set; } = string.Empty;
 
@@ -16,7 +16,7 @@ public class SignUp
 
     public string FreetradeCookie { get; set; } = string.Empty;
 
-    public SignUp()
+    public SignUpForm()
     {
         char[] validCharacters = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
         var random = new Random();
