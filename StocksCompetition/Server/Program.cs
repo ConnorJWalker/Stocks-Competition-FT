e88ff1d534c12c55eab45d92e553a43838f97f7b
Connor Walker <connorjwalker32@hotmail.co.uk>
2023-02-27 13:29:07 +0000

    init commit

---
+++ StocksCompetition/Server/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.ResponseCompression;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+WebApplication app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapControllers();
+app.MapFallbackToFile("index.html");
+
+app.Run();
