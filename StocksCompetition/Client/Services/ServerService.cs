f8e36cdf710685058906b139f57adb432a1fcb10
Connor Walker <connorjwalker32@hotmail.co.uk>
2023-03-05 23:18:25 +0000

    added appending token and getting account chart

--- StocksCompetition/Client/Services/ServerService.cs
+++ StocksCompetition/Client/Services/ServerService.cs
@@ -1,19 +1,25 @@
+using System.Net.Http.Headers;
 using System.Net.Http.Json;
+using Blazored.LocalStorage;
 using StocksCompetition.Shared;
+using StocksCompetition.Shared.Freetrade;
 
 namespace StocksCompetition.Client.Services;
 
 public class ServerService
 {
     private readonly HttpClient _httpClient;
-
-    public ServerService(IConfiguration configuration)
+    private readonly ILocalStorageService _localStorage;
+    
+    public ServerService(IConfiguration configuration, ILocalStorageService localStorage)
     {
         _httpClient = new HttpClient
         {
             BaseAddress = new Uri(configuration["ServerBaseAddress"] 
                 ?? throw  new MissingFieldException("No server base address provided"))
         };
+
+        _localStorage = localStorage;
     }
 
     public async Task<JwtResponse> SignUp(SignUpForm signUpForm)
@@ -23,18 +29,47 @@ public class ServerService
 
     public async Task<JwtResponse> LogIn(LogInForm logInFrom)
     {
-        return await SendPost<JwtResponse>("authentication/login", logInFrom);
+        return await SendPost<JwtResponse>("authentication/login", logInFrom, false);
     }
 
-    private async Task<T> SendPost<T>(string path, object content)
+    public async Task<AccountDetails> GetAccountChart()
     {
-        HttpResponseMessage response = await _httpClient.PostAsJsonAsync(path, content);
+        return await SendGet<AccountDetails>("freetrade/userchart");
+    }
+
+    private async Task<T> SendGet<T>(string path, bool withToken = true)
+    {
+        var request = new HttpRequestMessage(HttpMethod.Get, path);
+
+        HttpResponseMessage response = await SendRequest(request, withToken);
+        return (await response.Content.ReadFromJsonAsync<T>())!;
+    }
+    
+    private async Task<T> SendPost<T>(string path, object content, bool withToken = true)
+    {
+        var request = new HttpRequestMessage(HttpMethod.Post, path);
+        request.Content = JsonContent.Create(content);
+
+        HttpResponseMessage response = await SendRequest(request, withToken);
+        return (await response.Content.ReadFromJsonAsync<T>())!;
+    }
+
+    private async Task<HttpResponseMessage> SendRequest(HttpRequestMessage request, bool withToken = true)
+    {
+        if (withToken)
+        {
+            // Todo: handle refresh tokens
+            var token = await _localStorage.GetItemAsync<JwtResponse>("token");
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token.Token);
+        }
+
+        HttpResponseMessage response = await _httpClient.SendAsync(request);
         if (response is null || !response.IsSuccessStatusCode)
         {
-            // todo: not this
+            // Todo: handle failed responses better
             throw new Exception("Response was not success code");
         }
 
-        return (await response.Content.ReadFromJsonAsync<T>())!;
+        return await _httpClient.SendAsync(request);
     }
 }
