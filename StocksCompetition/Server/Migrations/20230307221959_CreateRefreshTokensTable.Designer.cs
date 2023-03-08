656864326909920f0353417c69d35a82dbf0f63b
Connor Walker <connorjwalker32@hotmail.co.uk>
2023-03-08 19:22:13 +0000

    added refresh token migration

--- StocksCompetition/Server/Migrations/20230307221959_CreateRefreshTokensTable.Designer.cs
+++ StocksCompetition/Server/Migrations/20230307221959_CreateRefreshTokensTable.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using StocksCompetition.Server.Entities;
 
@@ -10,9 +11,11 @@ using StocksCompetition.Server.Entities;
 namespace StocksCompetition.Server.Migrations
 {
     [DbContext(typeof(ServerDbContext))]
-    partial class ServerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230307221959_CreateRefreshTokensTable")]
+    partial class CreateRefreshTokensTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ StocksCompetition/Server/Migrations/20230307221959_CreateRefreshTokensTable.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StocksCompetition.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateRefreshTokensTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RefreshTokens",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(type: "varchar(64)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    RefreshToken = table.Column<string>(type: "varchar(64)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    ValidTo = table.Column<DateTimeOffset>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_refresh_token", x => x.RefreshToken);
+                    table.ForeignKey(
+                        name: "FK_user_id",
+                        column: x => x.UserId,
+                        principalTable: "users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade
+                    );
+                }
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(name: "RefreshTokens");
+        }
+    }
+}
